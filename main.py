6737272e1e14d9862ce64df58b4df548039aa8f2
vivek kumar <viveksnh933@gmail.com>
2026-03-06 16:07:48 +0530

    updated code

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ Base.metadata.create_all(bind=engine)
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://hrms-frontend-omega-woad.vercel.app"
 ]
 
 app.add_middleware(
